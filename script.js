d98f7d89e640d35c2caf2bed11b0a0ef507d69b2
Ujwal-1234 <56074956+Ujwal-1234@users.noreply.github.com>
2021-10-22 18:30:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function head1(){
+    document.getElementById('idm4').style.display = 'none';
+    document.getElementById('idm3').style.display = 'none';
+    document.getElementById('idm2').style.display = 'none';
+    document.getElementById('idm1').style.display = 'block';
+}
+function head2(){
+    document.getElementById('idm4').style.display = 'none';
+    document.getElementById('idm3').style.display = 'none';
+    document.getElementById('idm1').style.display = 'none';
+    document.getElementById('idm2').style.display = 'block';
+}
+
+function head3(){
+    document.getElementById('idm4').style.display = 'none';
+    document.getElementById('idm1').style.display = 'none';
+    document.getElementById('idm2').style.display = 'none';
+    document.getElementById('idm3').style.display = 'block';
+}
+
+function head4(){
+    document.getElementById('idm1').style.display = 'none';
+    document.getElementById('idm2').style.display = 'none';
+    document.getElementById('idm3').style.display = 'none';
+    document.getElementById('idm4').style.display = 'block';
+}
